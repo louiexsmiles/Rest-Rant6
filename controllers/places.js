c5dec57bdb24560e35c0cb96d8a77b70f95736a4
louiexsmiles <louismontes0101@gmail.com>
2022-09-18 13:41:46 -0400

    rest rant 6

---
+++ controllers/places.js
@@ -0,0 +1,56 @@
+const router = require('express').Router()
+const places = require('../models/places.js')
+//get the route that will eventually show the list of places
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/show', { place: places[id] })
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        places.splice(id, 1)
+        res.redirect('/places')
+    }
+})
+
+
+
+
+module.exports = router
